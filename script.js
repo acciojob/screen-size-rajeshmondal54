6d157a2a561b17626ae536fb23e1c2b3887f1b11
rajeshmondal54 <rm4223536@gmail.com>
2024-07-25 03:32:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+//your JS code here. If required.
 function updateSize() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    const sizeInfo = document.getElementById('sizeInfo').querySelector('h1');
-    sizeInfo.textContent = `Width: ${width} and Height: ${height}`;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const sizeInfo = document.getElementById('sizeInfo').querySelector('h1');
+  sizeInfo.textContent = `Width: ${width} and Height: ${height}`;
 }
-
 updateSize();
-
 window.addEventListener('resize', updateSize);
